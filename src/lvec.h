d6edb59c1e2b1a9007be326442aab1e9cc56b0df
djvanderlaan <djvanderlaan@unrealizedtime.nl>
2015-10-12 22:36:29 +0200

    Next idea

---
+++ src/lvec.h
@@ -0,0 +1,174 @@
+#ifndef lvec_h
+#define lvec_h
+
+#include <string>
+#include <stdexcept>
+#include <cstring>
+
+namespace ldat {
+
+  class dbl_lvec;
+  class int_lvec;
+  class str_lvec;
+
+  class lvec_visitor {
+    public:
+      virtual void visit(dbl_lvec& vec) = 0;
+      virtual void visit(int_lvec& vec) = 0;
+      virtual void visit(str_lvec& vec) = 0;
+  };
+
+
+  class lvec {
+    public:
+      typedef unsigned int vecsize;
+
+      lvec() {}
+      ~lvec() {}
+
+      virtual vecsize size() const = 0;
+      virtual double get_double(vecsize i) const = 0;
+      virtual int get_int(vecsize i) const = 0;
+      virtual bool get_bool(vecsize i) const = 0;
+
+      virtual void visit(lvec_visitor* visitor) = 0;
+  };
+
+  class dbl_lvec : public lvec {
+    public: 
+      dbl_lvec(vecsize size) : lvec(), size_(size) {
+        vec_ = new double[size];
+      }
+
+      ~dbl_lvec() {
+        delete [] vec_;
+      }
+
+      vecsize size() const {
+        return size_;
+      }
+
+      double get_double(vecsize i) const {
+        return vec_[i];
+      }
+
+      int get_int(vecsize i) const {
+        return vec_[i];
+      }
+
+      bool get_bool(vecsize i) const {
+        return vec_[i] == 0.0;
+      }
+
+      void visit(lvec_visitor* visitor) {
+        visitor->visit(*this);
+      }
+
+      double get(vecsize i) const {
+        return vec_[i];
+      }
+
+      void set(vecsize i, double value) {
+        vec_[i] = value;
+      }
+
+    private:
+      double* vec_;
+      vecsize size_;
+  };
+
+  class int_lvec : public lvec {
+    public: 
+      int_lvec(vecsize size) : lvec(), size_(size) {
+        vec_ = new int[size];
+      }
+
+      ~int_lvec() {
+        delete [] vec_;
+      }
+
+      vecsize size() const {
+        return size_;
+      }
+
+      double get_double(vecsize i) const {
+        return vec_[i];
+      }
+
+      int get_int(vecsize i) const {
+        return vec_[i];
+      }
+
+      bool get_bool(vecsize i) const {
+        return vec_[i] == 0.0;
+      }
+
+      void visit(lvec_visitor* visitor) {
+        visitor->visit(*this);
+      }
+
+      int get(vecsize i) const {
+        return vec_[i];
+      }
+
+      void set(vecsize i, int value) {
+        vec_[i] = value;
+      }
+
+    private:
+      int* vec_;
+      vecsize size_;
+  };
+
+
+  class str_lvec : public lvec {
+    public:
+      str_lvec(vecsize size, unsigned int strlen) : lvec(), size_(size), 
+          strlen_(strlen) {
+        vec_ = new char[size_ * strlen_];
+      }
+
+      ~str_lvec() {
+        delete [] vec_;
+      }
+
+      vecsize size() const {
+        return size_;
+      }
+
+      double get_double(vecsize i) const {
+        throw std::runtime_error("Can't convert a string to double.");
+      }
+
+      int get_int(vecsize i) const {
+        throw std::runtime_error("Can't convert a string to integer.");
+      }
+
+      bool get_bool(vecsize i) const {
+        throw std::runtime_error("Can't convert a string to boolean.");
+      }
+
+      void visit(lvec_visitor* visitor) {
+        visitor->visit(*this);
+      }
+
+      std::string get(vecsize i) const {
+        vecsize offset = i * strlen_;
+        return std::string(vec_ + offset);
+      }
+
+      void set(vecsize i, const std::string& str) {
+        vecsize offset = i * strlen_;
+        std::strncpy(vec_ + offset, str.c_str(), strlen_);
+        vec_[offset + strlen_ - 1] = 0;
+      }
+
+    private:
+      char* vec_;
+      vecsize size_;
+      unsigned int strlen_;
+  };
+
+};
+
+#endif
---
+++ src/test_lvec.cpp
@@ -0,0 +1,81 @@
+
+#include "cppr.h"
+#include "lvec.h"
+#include <iostream>
+
+// Pair of macros that can be used to catch any remaining exceptions and pass
+// these on to R. Start and end functions that get called by R by these.
+#define RTRY \
+    try {
+#define RCATCH \
+    } catch(const std::string& e) { \
+      error(e.c_str()); \
+      return R_NilValue; \
+    } catch(const std::exception& e) { \
+      error(e.what()); \
+      return R_NilValue; \
+    } catch (...) { \
+      error("Uncaught exception."); \
+      return R_NilValue; \
+    } \
+
+
+class assign_visitor : public ldat::lvec_visitor {
+  public: 
+    assign_visitor(ldat::lvec& index, ldat::lvec& values) : index_(index), values_(values) {
+    }
+
+    void visit(ldat::dbl_lvec& vec) {
+     // TODO check sizes
+     for (ldat::lvec::vecsize i = 0; i < index_.size(); ++i) {
+       int index = index_.get_int(i);
+       double value = values_.get_double(i);
+       vec.set(index, value);
+     }
+    }
+
+    void visit(ldat::int_lvec& vec) {
+     for (ldat::lvec::vecsize i = 0; i < index_.size(); ++i) {
+       int index = index_.get_int(i);
+       int value = values_.get_int(i);
+       vec.set(index, value);
+     }
+    }
+
+    void visit(ldat::str_lvec& vec) {
+    }
+
+  private:
+    ldat::lvec& index_;
+    ldat::lvec& values_;
+
+
+};
+
+extern "C" {
+  SEXP test_lvec2(SEXP rp) {
+    RTRY
+
+    ldat::dbl_lvec v1(10);
+    ldat::int_lvec v2(10);
+    ldat::dbl_lvec v3(10);
+
+    for (ldat::lvec::vecsize i = 0; i < v1.size(); ++i) {
+      v1.set(i, i);
+      v2.set(i, 10-i);
+      v3.set(i, i*i);
+    }
+
+    assign_visitor visitor(v2, v3);
+    v1.visit(&visitor);
+
+    for (ldat::lvec::vecsize i = 0; i < v1.size(); ++i) {
+      std::cout << v1.get(i) << "\t\t" << v2.get(i) <<
+        "\t\t" << v3.get(i) << "\n";
+    }
+
+    return R_NilValue;
+    RCATCH
+  }
+}
+
