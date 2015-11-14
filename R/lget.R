c005f14bcf4190f94b48b8df0080f90c00bf1cb8
djvanderlaan <djvanderlaan@unrealizedtime.nl>
2015-11-14 16:56:53 +0100

    Added range indexing

--- R/lget.R
+++ R/lget.R
@@ -1,12 +1,22 @@
 
 
-# TODO: range checkingls
-
 #' @useDynLib ldat2
 #' @export
-lget <- function(x, index) {
-  index <- as.lvec(index)
+lget <- function(x, index = NULL, range = NULL) {
   if (!is.lvec(x)) stop("x should be of type lvec.")
-  res <- .Call("get", x, index)
-  structure(res, class="lvec")
+
+  if (!is.null(index)) {
+    if (!is.null(range))
+      warning("Both range and index specified. Ignoring range.")
+    index <- as.lvec(index)
+    res <- .Call("get", x, index)
+    structure(res, class="lvec")
+  } else if (!is.null(range)) {
+    range <- as.numeric(range)
+    res <- .Call("get_range", x, range)
+    structure(res, class="lvec")
+  } else {
+    stop("Neither index nor range are specified.")
+  }
 }
+
--- src/indexing_range.cpp
+++ src/indexing_range.cpp
@@ -4,10 +4,22 @@
 
 class range_indexing_visitor : public ldat::lvec_visitor {
   public: 
-    range_indexing_visitor(ldat::vec& index) : index_(index), result_(0) { }
+    range_indexing_visitor(ldat::vec::vecsize lower, ldat::vec::vecsize upper) : lower_(lower),
+        upper_(upper), result_(0) { } 
 
     template<typename T>
     void visit_template(ldat::lvec<T>& vec) {
+      if (lower_ < 0) throw std::runtime_error("Index out of range.");
+      if (upper_ >= vec.size()) throw std::runtime_error("Index out of range.");
+      if (upper_ < lower_) throw std::runtime_error("Range has negative length.");
+      ldat::vec::vecsize size = upper_ - lower_ + 1;
+      std::unique_ptr<ldat::lvec<T> > result(new ldat::lvec<T>(size, vec));
+      ldat::vec::vecsize j = 0;
+      for (ldat::vec::vecsize i = lower_; i <= upper_; ++i, ++j) {
+        T value = vec.get(i);
+        result->set(j, value);
+      }
+      result_ = result.release();
     }
 
     void visit(ldat::lvec<double>& vec) {
@@ -31,26 +43,34 @@ class range_indexing_visitor : public ldat::lvec_visitor {
     }
 
   private:
-    ldat::vec& index_;
+    ldat::vec::vecsize upper_;
+    ldat::vec::vecsize lower_;
     ldat::vec* result_;
 };
 
-// TODO: range index
 extern "C" {
   SEXP get_range(SEXP rv, SEXP rindex) {
     CPPRTRY
     cppr::rvec<cppr::numeric> index{rindex};
+     
+    // check input
     if (index.length() != 2)
       throw std::runtime_error("Expecting vector of length 2 for range index.");
-    if (index[1] < index[0])
-      throw std::runtime_error("Top of range is smaller than bottom.");
-    return R_NilValue;
+    if (cppr::is_na(index[0]))
+      throw std::runtime_error("Missing value for lower bound of range.");
+    if (cppr::is_na(index[0]))
+      throw std::runtime_error("Missing value for upper bound of range.");
+      
+    range_indexing_visitor visitor{static_cast<ldat::vec::vecsize>(index[0]-1), 
+      static_cast<ldat::vec::vecsize>(index[1]-1)};
+    ldat::vec* v = sexp_to_vec(rv);
+    v->visit(&visitor);
+    return vec_to_sexp(visitor.result());
+    
 
     //ldat::vec* index = sexp_to_vec(rindex);
-    //indexing_visitor visitor{*index};
     //ldat::vec* v = sexp_to_vec(rv);
     //v->visit(&visitor);
-    //return vec_to_sexp(visitor.result());
     CPPRCATCH
   }
 }
