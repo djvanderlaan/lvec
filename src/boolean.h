85b1489a19d787647ef18c2864ce6d83dd1c9d1f
djvanderlaan <djvanderlaan@unrealizedtime.nl>
2016-10-19 18:07:19 +0200

    Comments

--- src/boolean.h
+++ src/boolean.h
@@ -11,28 +11,30 @@ namespace cppr {
         if (cppr::is_na(val)) val_ = 2;
       }
       boolean(bool val) : val_(val ? 1 : 0) {}
-      
+
       bool na() const { return val_ == 2;}
       void na(bool na) { val_ = na ? 2 : 0;}
       void set(bool val) { val_ = val ? 1 : 0;}
 
       boolean& operator=(bool bl) {
-	set(bl);
-	return *this;
+	      set(bl);
+	      return *this;
       }
 
       boolean& operator=(const boolean& bl) {
-	val_ = bl.val_;
-	return *this;
+        // TODO: comparison of NA'sshould return NA????
+	      val_ = bl.val_;
+	      return *this;
       }
 
       operator bool() const { return val_ == 1;}
+      // TODO: shouldn't next functions return NA in case of val_ == 2???
       operator double() const { return val_ == 1 ? 1.0 : 0.0;}
       operator int() const { return val_ == 1 ? 1 : 0;}
 
     private:
       unsigned char val_;
-  }; 
+  };
 
   inline bool is_na(const boolean& x) { return x.na();}
   inline bool is_nan(const boolean& x) { return x.na();}
@@ -42,10 +44,10 @@ namespace cppr {
   inline int base_type(const boolean& v) { return int();}
   inline std::string base_type(const std::string& v) { return std::string();}
 
-  inline bool operator<(const boolean& lhs, const boolean& rhs)  { 
+  inline bool operator<(const boolean& lhs, const boolean& rhs)  {
     if (lhs.na()) return false;
     if (rhs.na()) return true;
-    return static_cast<bool>(lhs) < static_cast<bool>(rhs); 
+    return static_cast<bool>(lhs) < static_cast<bool>(rhs);
   }
 
 }
