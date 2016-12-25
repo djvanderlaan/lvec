37f5f10d20403b6723bd42e825efd739e4b28b7a
djvanderlaan <djvanderlaan@unrealizedtime.nl>
2016-12-25 17:30:57 +0100

    Added documentation lset

--- R/lset.R
+++ R/lset.R
@@ -1,5 +1,28 @@
 
-
+#' Set values in an lvec
+#' 
+#' @param x \code{\link{lvec}} to set values in
+#' @param index a numeric or logical vector with indices at which the values
+#'   should be set. 
+#' @param value a vector with the new values. When shorter than the length of
+#'   the indices the values are recycled.
+#'
+#' @details
+#' Should behave in the same way as assigning and indexing to a regular
+#' R-vector. 
+#'
+#' @examples
+#' a <- lvec(1:10)
+#' # set second element to 20
+#' lset(a, 2, 20)
+#' print(a)
+#' # set odd elements to 20
+#' lset(a, c(TRUE, FALSE), 20)
+#' print(a)
+#' # values are recycled
+#' lset(a, 1:4, 100:101)
+#' print(a)
+#' 
 #' @useDynLib lvec
 #' @export
 lset <- function(x, index, values) {
