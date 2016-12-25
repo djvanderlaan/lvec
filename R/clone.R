26d25816ac9f1bf8b1aad0fa0eb5f8d0db04fb17
djvanderlaan <djvanderlaan@unrealizedtime.nl>
2016-12-25 08:58:48 +0100

    Documentation clone+lget

--- R/clone.R
+++ R/clone.R
@@ -1,5 +1,29 @@
 
-
+#' Clone an lvec object
+#'
+#' @param x \code{\link{lvec}} object to clone
+#'
+#' @details
+#' \code{\link{lvec}} objects are basically pointers to pieces of memory. 
+#' When copying an object only the pointer is copied and when modifying the copied
+#' object also the original object is modified. The advantage of this is speed: 
+#' these is less copying of the complete vector. In order to obtain a true copy
+#' of an \code{lvec} code can be used.
+#'
+#' @examples
+#' a <- as_lvec(1:3)
+#' # Copy
+#' b <- a
+#' # When modifying the copy also the original is modified
+#' lset(b, 1, 10)
+#' print(a)
+#' print(b)
+#' # Use clone to make a true copy
+#' b <- clone(a)
+#' lset(b, 1, 100)
+#' print(a)
+#' print(b)
+#'
 #' @useDynLib lvec
 #' @export
 clone <- function(x) {
--- R/lget.R
+++ R/lget.R
@@ -1,5 +1,31 @@
 
-
+#' Read elemements from an lvec
+#'
+#' @param x the \code{\link{lvec}} to read from
+#' @param index a logical or numeric vector to index \code{x} with
+#' @param range a numeric vector of length 2 specifying a range of elements 
+#'   to select. Specify either \code{index} or \code{range}. 
+#'
+#' @details
+#' Indexing using \code{index} should follow the same rules as indexing a regular
+#' R-vector using a logical or numeric index. The range given by \code{range} 
+#' includes both end elements. So, a range of \code{c(1,3)} selects the first 
+#' three elements. 
+#'
+#' @return
+#' Returns an \code{\link{lvec}} with the selected elements. In order to convert
+#' the selection to an R-vector \code{\link{as_rvec}} can e used.
+#'
+#' @examples
+#' a <- as.lvec(letters[1:4])
+#' # Select first two elements
+#' lget(a, 1:2)
+#' lget(a, c(TRUE, TRUE, FALSE, FALSE))
+#' lget(a, range = c(1,2))
+#'
+#' # Logical indices are recycled: select odd elements
+#' lget(a, c(TRUE, FALSE))
+#' 
 #' @useDynLib lvec
 #' @export
 lget <- function(x, index = NULL, range = NULL) {
