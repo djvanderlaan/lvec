1a2b0203b7b93143b37080ea7b1d4070041a20a7
djvanderlaan <djvanderlaan@unrealizedtime.nl>
2017-05-17 17:02:18 +0200

    Converted chunk to S3 method

--- R/chunk.R
+++ R/chunk.R
@@ -1,4 +1,11 @@
 
+#' @rdname chunk
+#' @export
+chunk <- function(x, ...) {
+  UseMethod("chunk")
+}
+
+
 #' Generate a number of index ranges from a vector 
 #' 
 #' The ranges have a maximum length.
@@ -8,8 +15,17 @@
 #'   or a regular R vector. 
 #' @param chunk_size a numeric vector of length 1 giving the maximum length of
 #'   the chunks. 
+#' @param ... ignores; used to pass additional arguments to other methods.  
+#' 
+#' @details 
+#' The default chunk size can be changes by setting the option 'chunk_size', 
+#' (`options(chunk_size = <new default chunk size>)`).
+#' 
+#' Implementations of chunk for data frames and regular vectors are provided to
+#' make it easier to write code that works on both lvec objects and regular R
+#' objects. 
 #'   
-#'   
+#' @rdname chunk
 #' @export
 chunk <- function(x, chunk_size = 1E6) {
   nchunks <- ceiling(length(x) / chunk_size)
@@ -19,4 +35,26 @@ chunk <- function(x, chunk_size = 1E6) {
   res <- split(cbind(start, end), seq_len(nchunks))
   names(res) <- NULL
   res
+}
+
+#' @rdname chunk
+#' @export
+chunk.default <- function(x, chunk_size = NULL, ...) {
+  if (is.null(chunk_size)) chunk_size <- length(x)
+  nchunks <- ceiling(length(x) / chunk_size)
+  pos <- seq(1, length(x)+1, length.out = nchunks+1)
+  start <- pos[seq_len(length(pos)-1)]
+  end   <- pos[seq_len(length(pos)-1) + 1] - 1
+  split(cbind(start, end), seq_len(nchunks))
+}
+
+#' @rdname chunk
+#' @export
+chunk.data.frame <- function(x, chunk_size = NULL, ...) {
+  if (is.null(chunk_size)) chunk_size <- length(x)
+  nchunks <- ceiling(nrow(x) / chunk_size)
+  pos <- seq(1, nrow(x)+1, length.out = nchunks+1)
+  start <- pos[seq_len(length(pos)-1)]
+  end   <- pos[seq_len(length(pos)-1) + 1] - 1
+  split(cbind(start, end), seq_len(nchunks))
 }
