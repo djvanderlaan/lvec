d27c0e1bdfa012622954eb3ce99eb95046410f1b
djvanderlaan <djvanderlaan@unrealizedtime.nl>
2017-05-17 08:01:21 +0200

    Changed lget to S3 method

--- R/lget.R
+++ R/lget.R
@@ -1,10 +1,20 @@
 
+
+#' @rdname lget
+#' @export
+lget <- function(x, ...) {
+  UseMethod('lget')
+}
+
+
+
 #' Read elemements from an lvec
 #'
 #' @param x the \code{\link{lvec}} to read from
 #' @param index a logical or numeric vector to index \code{x} with
 #' @param range a numeric vector of length 2 specifying a range of elements 
 #'   to select. Specify either \code{index} or \code{range}. 
+#' @param ... used to pass on additional arguments to other methods.
 #'
 #' @details
 #' Indexing using \code{index} should follow the same rules as indexing a regular
@@ -27,8 +37,9 @@
 #' lget(a, c(TRUE, FALSE))
 #' 
 #' @useDynLib lvec
+#' @rdname lget
 #' @export
-lget <- function(x, index = NULL, range = NULL) {
+lget.lvec <- function(x, index = NULL, range = NULL, ...) {
   if (!is_lvec(x)) stop("x should be of type lvec.")
 
   if (!is.null(index)) {
@@ -48,3 +59,24 @@ lget <- function(x, index = NULL, range = NULL) {
   }
 }
 
+#' @rdname lget
+#' @export
+lget.default <- function(x, index = NULL, range = NULL, ...) {
+  if (!is.null(range)) {
+    if (!is.null(index))
+      warning("Both range and index specified. Ignoring index.")
+    index <- seq.int(range[1], range[2], by = 1)
+  }
+  x[index]
+}
+
+#' @rdname lget
+#' @export
+lget.data.frame <- function(x, index = NULL, range = NULL, ...) {
+  if (!is.null(range)) {
+    if (!is.null(index))
+      warning("Both range and index specified. Ignoring index.")
+    index <- seq.int(range[1], range[2], by = 1)
+  }
+  x[index, , drop = FALSE]
+}
