1a1de6c73a2ddd60150d47609740b33d3360e535
djvanderlaan <djvanderlaan@unrealizedtime.nl>
2017-07-11 14:47:24 +0200

    Register routines missing header

---
+++ src/r_export.h
@@ -0,0 +1,19 @@
+#include "ldat.h"
+
+extern "C" {
+  SEXP as_lvec(SEXP rv);
+  SEXP as_rvec(SEXP rv);
+  SEXP assign(SEXP rv, SEXP rindex, SEXP rvalues);
+  SEXP assign_range(SEXP rv, SEXP rindex, SEXP rvalues);
+  SEXP lclone(SEXP rv);
+  SEXP new_lvec(SEXP rsize, SEXP rtype, SEXP rstrlen);
+  SEXP get_type(SEXP rv);
+  SEXP get(SEXP rv, SEXP rindex);
+  SEXP get_range(SEXP rv, SEXP rindex);
+  SEXP order(SEXP rv);
+  SEXP get_size(SEXP rv);
+  SEXP set_size(SEXP rv, SEXP rsize);
+  SEXP sort(SEXP rv);
+  SEXP get_strlen(SEXP rv);
+  SEXP set_tempdir(SEXP rd);
+}
